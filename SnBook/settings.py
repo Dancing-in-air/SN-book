e3bb7e384c61e2e5c87a0e128e800dd1b91bf4ac
Dancing-in-air <513029b001g.cdb@sina.cn>
2019-11-15 09:48:10 +0800

    201911150948

--- SnBook/settings.py
+++ SnBook/settings.py
@@ -13,14 +13,14 @@ BOT_NAME = 'SnBook'
 
 SPIDER_MODULES = ['SnBook.spiders']
 NEWSPIDER_MODULE = 'SnBook.spiders'
-LOG_LEVEL = "WARNING"
+# LOG_LEVEL = "WARNING"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- SnBook/spiders/book.py
+++ SnBook/spiders/book.py
@@ -10,6 +10,12 @@ class BookSpider(scrapy.Spider):
     allowed_domains = ['suning.com']
     start_urls = ['https://book.suning.com']
 
+    def start_requests(self):
+        # 重构star_requests方法,添加cookies
+        cookies = "tradeMA=32; _snvd=1573103014556ZKDu8K46pHy; SN_CITY=10_010_1000000_9017_01_10106_4_0; cityId=9017; districtId=10106; hm_guid=01681949-b0ec-40d2-98a4-b98366891654; _device_session_id=p_5af1172f-f005-4372-88c4-d94f25d79f19; _df_ud=87c3e180-3aea-45dd-878b-b259cdfdd037; smhst=646493062|0070167435a10547672246|0070153938a10820148331|0070170340a107672198|0070151041a173041855|0070091633a10583295404|0070213869a10570782544|0070170340a11097919507|0070418556a10065021930|0070078847a10717510914|0070091633; authId=siBDFE536E42BE78A56FBF048E838E9753; secureToken=90BC857D6872B6FAFBD2C26A0E3A3414; _snmc=1; _snsr=baidu%7Cbrand%7C%7Ctitle%7C%25E8%258B%258F%25E5%25AE%2581%25E6%2598%2593%25E8%25B4%25AD*%3A*; _snma=1%7C156980518612672314%7C1573103008705%7C1573782147138%7C1573782148877%7C153%7C20; _snmp=157378214322542305; _snmb=157378214888420447%7C1573782148896%7C1573782148884%7C1; _snzwt=TH8kT516e6cb9da06kIex53d4"
+        cookies = {i.split("=")[0].strip(): i.split("=")[1].strip() for i in cookies.split(";")}
+        yield scrapy.Request(self.start_urls[0], callback=self.parse, cookies=cookies)
+
     def parse(self, response):
         menu_list = response.xpath("//div[@class='menu-list']/div[@class='menu-item']")
         for menu in menu_list:
